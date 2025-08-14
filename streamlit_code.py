4d59b440dc6fd4760b9edbf26f214559acbc78fe
Chethan jayashankar <142323654+Chethancj@users.noreply.github.com>
2025-08-14 18:17:02 +0100

    Update streamlit_code.py

--- streamlit_code.py
+++ streamlit_code.py
@@ -772,4 +772,4 @@ public_url = ngrok.connect(addr=8501)
 print(f" App running at: {public_url}")
 
 # Run the Streamlit app in the background
-!streamlit run app.py --server.port 8501 &>/dev/null&
+
